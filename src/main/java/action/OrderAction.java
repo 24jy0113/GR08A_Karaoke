115e22109a9441a0fa5607ef50621facc7b47dd5
24jy0136 <24jy0136@C12.jynet.jec.ac.jp>
2025-12-24 14:54:07 +0900

    acrtionのOrderActionを追加しました。

--- src/main/java/action/OrderAction.java
+++ src/main/java/action/OrderAction.java
@@ -1,5 +1,23 @@
 package action;
 
-public class OrderAction {
+import java.util.ArrayList;
 
+import model.Order;
+import model.OrderItem;
+
+public class OrderAction {
+	public Order createOrder(ArrayList<OrderItem> items, int room, boolean deliver) {
+	}
+
+	public void addOrder(Order order) {
+	}
+
+	public ArrayList<Order> searchOrderByRoom(int room) {
+	}
+
+	public ArrayList<Order> searchOrderByStatus(String status) {
+	}
+
+	public void setOrderStatus(Order order, String status) {
+	}
 }
