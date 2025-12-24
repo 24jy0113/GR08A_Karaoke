6ef8917b97ffa4e047641461090ac8612d357dd9
24jy0136 <24jy0136@C12.jynet.jec.ac.jp>
2025-12-24 14:50:27 +0900

    modelのItemとOrderとOrderItemを追加しました。

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,5 +1,66 @@
 package model;
 
-public class Order {
+import java.util.ArrayList;
 
+public class Order {
+	private int id;
+	private ArrayList<OrderItem> item;
+	private int total;
+	private int roomId;
+	private int room;
+	private int receivingNo;
+	private int statusId;
+	private String status;
+
+	public Order(int id, ArrayList<OrderItem> item, int total, int roomId, int room, int receivingNo, int statusId,
+			String status) {
+		this.id = id;
+		this.item = item;
+		this.total = total;
+		this.room = room;
+		this.receivingNo = receivingNo;
+		this.statusId = statusId;
+		this.status = status;
+	}
+
+	public Order(ArrayList<OrderItem> item, int roomId, int room, int receivingNo, int statusId, String status) {
+		this(-1, item, 0, roomId, room, receivingNo, statusId, status);
+		int sum = 0;
+		for (int i = 0; i < item.size(); i++) {
+			sum += item.get(i).getTotal();
+		}
+		total = sum;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public ArrayList<OrderItem> getItem() {
+		return item;
+	}
+
+	public int getReceivingNo() {
+		return receivingNo;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getRoom() {
+		return room;
+	}
+
+	public int getTotal() {
+		return total;
+	}
 }
--- src/main/java/model/OrderItem.java
+++ src/main/java/model/OrderItem.java
@@ -1,5 +1,99 @@
 package model;
 
 public class OrderItem {
+	private int id;
+	private Item item;
+	private int[] option;
+	private int count;
+	private int total;
 
+	public OrderItem(Item item) {
+		this(item, 1, null);
+	}
+
+	public OrderItem(Item item, int[] option) {
+		this(item, 1, option);
+	}
+
+	public OrderItem(Item item, int count) {
+		this(item, count, null);
+	}
+
+	public OrderItem(Item item, int count, int[] option) {
+		this(-1, item, count, option, 0);
+		calcTotal();
+	}
+
+	public OrderItem(int id, Item item, int count, int[] option, int total) {
+		this.id = id;
+		this.item = item;
+		this.count = count;
+		this.option = option;
+		this.total = total;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		setItem(item, 1);
+		calcTotal();
+	}
+
+	public void setItem(Item item, int count) {
+		this.item = item;
+		this.count = count;
+		calcTotal();
+	}
+
+	public int[] getOptionList() {
+		return option;
+	}
+
+	public int getOption(int i) {
+		return option[i];
+	}
+
+	public void setOptionList(int[] option) {
+		this.option = option;
+		calcTotal();
+	}
+
+	public void setOption(int i, int option) {
+		this.option[i] = option;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+		calcTotal();
+	}
+
+	private void calcTotal() {
+		int optionSum = 0;
+		for (int i = 0; i < option.length; i++) {
+			optionSum += item.getOptionPrice(i);
+		}
+		total = (item.getPrice() + optionSum) * count;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
 }
