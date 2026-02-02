e0b5ad1b87e905aecd4a4a4653d5db130365363f
24jy0125 <24jy0125@C13.jynet.jec.ac.jp>
2026-02-02 12:24:48 +0900

    延長確認のExtendConfirmServlet.javaを追加

---
+++ src/main/java/controller/ExtendConfirmServlet.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import action.ExtendAction;
+
+@WebServlet("/ExtendConfirmServlet")
+public class ExtendConfirmServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ExtendConfirmServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+
+		int extendMinutes = (Integer) session.getAttribute("extendMinutes");
+
+		try {
+			new ExtendAction().execute(extendMinutes, session);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+		// 完了画面へ行く.
+		response.sendRedirect(request.getContextPath() + "/time_extend_confirmed.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
