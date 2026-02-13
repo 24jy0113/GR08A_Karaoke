e3d2a6d1aeb79a6e669e5592f475d0b1959fde3c
24jy0136 <24jy0136@C12.jynet.jec.ac.jp>
2026-02-13 10:26:42 +0900

    酒類の提供不可の部屋の場合にアルコールカテゴリーの商品番号を入れるとエラーメッセージが出るようにしました。

--- src/main/java/controller/ItemSearchServlet.java
+++ src/main/java/controller/ItemSearchServlet.java
@@ -1,11 +1,16 @@
 package controller;
 
-import dao.ItemDao;
-import model.Item;
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
-import java.io.IOException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import dao.ItemDao;
+import model.Item;
+import model.Room;
 @WebServlet("/ItemSearchServlet")
 public class ItemSearchServlet extends HttpServlet {
 
@@ -24,19 +29,29 @@ public class ItemSearchServlet extends HttpServlet {
 
             int orderNumber = Integer.parseInt(numStr);
 
-            // ② DAO で検索
+            // DAO で検索
             ItemDao dao = new ItemDao();
             Item item = dao.searchItemByNumber(orderNumber);
 
-            // ③ 見つからない場合
+            // 見つからない場合
             if (item == null) {
                 req.setAttribute("error", "該当する商品がありません");
                 req.getRequestDispatcher("/item_search.jsp")
                        .forward(req, res);
                 return;
             }
+            
+            var session = req.getSession();
+            var room=(Room)(session.getAttribute("room"));
+            
+            if (item.getCategoryId()==1&&!room.isAlcohol()) {
+                req.setAttribute("error", "受付時の設定により、アルコール商品は注文できません");
+                req.getRequestDispatcher("/item_search.jsp")
+                       .forward(req, res);
+                return;            	
+            }
 
-            // ④ 売り切れ判定
+            // 売り切れ判定
             if (!item.isStock()) {
                 req.setAttribute("item", item);
                 req.setAttribute("error", "売り切れのため注文できません");
@@ -44,7 +59,7 @@ public class ItemSearchServlet extends HttpServlet {
                 return;
             }
 
-            // ⑤ 商品を次画面へ
+            // 商品を次画面へ
             req.setAttribute("item", item);
             req.getRequestDispatcher("/item_detail.jsp")
                    .forward(req, res);
