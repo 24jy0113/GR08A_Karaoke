7fdef44f7a7b06ea0242ce9d00c5c71e4669e937
24jy0136 <24jy0136@C12.jynet.jec.ac.jp>
2026-02-16 15:32:07 +0900

    使ってないメソッドを削除しました

--- src/main/java/dao/DatabaseManager.java
+++ src/main/java/dao/DatabaseManager.java
@@ -2,7 +2,6 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DatabaseManager {
 	private static String url = "jdbc:mysql://10.64.144.5:3306/24jy0125";
@@ -22,26 +21,5 @@ public class DatabaseManager {
 
 	protected static Connection connect() throws Exception {
 		return DriverManager.getConnection(url, user, pass);
-	}
-
-	protected static void close(Connection con) throws Exception {
-		try {
-			// 受け取った接続がnullではない場合.
-			if (con != null) {
-				// データベースとの接続を切断する.
-				con.close();
-			}
-		} catch (SQLException e) {
-
-			// デバッグ用のスタックトレース.
-			e.printStackTrace();
-
-			// フロントエンド用のエラーメッセージ.
-			String errMsg = "DB接続に失敗しました！<br>管理者に連絡してください。";
-
-			// 例外を投げる
-			throw new Exception(errMsg);
-
-		}
 	}
 }
